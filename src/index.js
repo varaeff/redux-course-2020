599c4664817c692b2b638fb1b745cee37feea9e8
varaeff <varaev.sergey@gmail.com>
2024-02-14 19:48:23 +0300

    final version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-import './styles.css'
+import "./styles.css";
+import { applyMiddleware, createStore, compose } from "redux";
+import { thunk } from "redux-thunk";
+import { rootReducer } from "./redux/rootReducer";
+import {
+  asyncIncrement,
+  changeTheme,
+  decrement,
+  increment,
+} from "./redux/actions";
+import logger from "redux-logger";
+
+const counter = document.getElementById("counter");
+const addBtn = document.getElementById("add");
+const subBtn = document.getElementById("sub");
+const asyncBtn = document.getElementById("async");
+const themeBtn = document.getElementById("theme");
+
+// function logger(state) {
+//   return function (next) {
+//     return function (action) {
+//       console.log("prevState: ", state.getState());
+//       console.log("Action: ", action);
+//       const newState = next(action);
+//       console.log("newState: ", newState);
+//       return newState;
+//     };
+//   };
+// }
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk, logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+addBtn.addEventListener("click", () => {
+  store.dispatch(increment());
+});
+
+subBtn.addEventListener("click", () => {
+  store.dispatch(decrement());
+});
+
+asyncBtn.addEventListener("click", () => {
+  store.dispatch(asyncIncrement());
+});
+
+themeBtn.addEventListener("click", () => {
+  const newTheme = document.body.classList.contains("light") ? "dark" : "light";
+  store.dispatch(changeTheme(newTheme));
+});
+
+store.subscribe(() => {
+  const state = store.getState();
+  counter.textContent = state.counter;
+  document.body.className = state.theme.value;
+  [addBtn, subBtn, themeBtn, asyncBtn].forEach(
+    (btn) => (btn.disabled = state.theme.disabled)
+  );
+});
+
+store.dispatch({ type: "INIT_APP" });
